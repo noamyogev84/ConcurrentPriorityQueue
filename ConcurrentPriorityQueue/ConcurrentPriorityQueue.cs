68754564e980c08a8d7d95ae058a56d6e8efd8c2
Noam <noamyogev84@gmail.com>
2019-07-10 23:47:55 +0300

    add IEnumerable unit tests

--- ConcurrentPriorityQueue/ConcurrentPriorityQueue.cs
+++ ConcurrentPriorityQueue/ConcurrentPriorityQueue.cs
@@ -6,111 +6,111 @@ using CSharpFunctionalExtensions;
 
 namespace ConcurrentPriorityQueue
 {
-	public class ConcurrentPriorityQueue<T> : IConcurrentPriorityQueue<T> where T : IHavePriority
-	{
-		private readonly Dictionary<int, Queue<T>> _internalQueues;
-		private readonly object _syncRoot = new object();
+    public class ConcurrentPriorityQueue<T> : IConcurrentPriorityQueue<T> where T : IHavePriority
+    {
+        private readonly Dictionary<int, Queue<T>> _internalQueues;
+        private readonly object _syncRoot = new object();
 
-		public int Count => _internalQueues.Values.Select(q => q.Count).Aggregate((a, b) => a + b);
+        public int Count => _internalQueues.Values.Select(q => q.Count).Aggregate((a, b) => a + b);
 
-		public bool IsSynchronized => true;
+        public bool IsSynchronized => true;
 
-		public object SyncRoot => _syncRoot;
+        public object SyncRoot => _syncRoot;
 
-		public ConcurrentPriorityQueue(int supportedNumberOfPriorites = 1)
-		{
-			_internalQueues = Enumerable.Range(0, supportedNumberOfPriorites)
-				.ToDictionary(k => k, v => new Queue<T>());
-		}
+        public ConcurrentPriorityQueue(int supportedNumberOfPriorites = 1)
+        {
+            _internalQueues = Enumerable.Range(0, supportedNumberOfPriorites)
+                .ToDictionary(k => k, v => new Queue<T>());
+        }
 
-		public void CopyTo(T[] array, int index)
-		{
-			var itemsArray = ToArray();
-			lock (_syncRoot)
-				itemsArray.CopyTo(array, index);
-		}
+        public void CopyTo(T[] array, int index)
+        {
+            var itemsArray = ToArray();
+            lock (_syncRoot)
+                itemsArray.CopyTo(array, index);
+        }
 
-		public void CopyTo(Array array, int index) => CopyTo((T[])array, index);
-		
-		public T[] ToArray()
-		{
-			lock (_syncRoot)
-			{
-				var itemsList = new List<T>();
-				foreach (var queue in _internalQueues.Values)
-				{
-					itemsList.AddRange(queue.ToArray());
-				}
+        public void CopyTo(Array array, int index) => CopyTo((T[])array, index);
+        
+        public T[] ToArray()
+        {
+            lock (_syncRoot)
+            {
+                var itemsList = new List<T>();
+                foreach (var queue in _internalQueues.Values)
+                {
+                    itemsList.AddRange(queue.ToArray());
+                }
 
-				return itemsList.ToArray();
-			}
-		}
+                return itemsList.ToArray();
+            }
+        }
 
-		public bool TryAdd(T item) => Enqueue(item).IsSuccess;
+        public bool TryAdd(T item) => Enqueue(item).IsSuccess;
 
-		public bool TryTake(out T item)
-		{			
-			var result = Dequeue();
-			lock (_syncRoot)
-			{
-				item = default(T);
-				if (result.IsFailure)
-					return false;
+        public bool TryTake(out T item)
+        {           
+            var result = Dequeue();
+            lock (_syncRoot)
+            {
+                item = default(T);
+                if (result.IsFailure)
+                    return false;
 
-				item = result.Value;
-				return true;
-			}			
-		}
+                item = result.Value;
+                return true;
+            }           
+        }
 
-		public IEnumerator<T> GetEnumerator() => ToArray().GetEnumerator() as IEnumerator<T>;
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)ToArray()).GetEnumerator();
 
-		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-		public Result Enqueue(T item)
-		{
-			lock (_syncRoot)
-			{
-				if (!_internalQueues.ContainsKey(item.Priority))
-					return Result.Fail("Failed to enqueue item");
+        public Result Enqueue(T item)
+        {
+            lock (_syncRoot)
+            {
+                if (!_internalQueues.ContainsKey(item.Priority))
+                    return Result.Fail("Failed to enqueue item");
 
-				_internalQueues[item.Priority].Enqueue(item);
-				return Result.Ok();
-			}			
-		}
+                _internalQueues[item.Priority].Enqueue(item);
+                return Result.Ok();
+            }           
+        }
 
-		public Result<T> Dequeue()
-		{
-			lock (_syncRoot)
-			{
-				return GetNextQueue()
-					.OnSuccess(q => q.Dequeue())
-					.OnFailure(err => Result.Fail(err));
-			}
-		}
+        public Result<T> Dequeue()
+        {
+            lock (_syncRoot)
+            {
+                return GetNextQueue()
+                    .OnSuccess(q => q.Dequeue())
+                    .OnFailure(err => Result.Fail(err));
+            }
+        }
 
-		public Result<T> Peek()
-		{
-			lock (_syncRoot)
-			{
-				return GetNextQueue()
-					.OnSuccess(q => q.Peek())
-					.OnFailure(err => Result.Fail(err));
-			}
-		}
+        public Result<T> Peek()
+        {
+            lock (_syncRoot)
+            {
+                return GetNextQueue()
+                    .OnSuccess(q => q.Peek())
+                    .OnFailure(err => Result.Fail(err));
+            }
+        }
 
-		private Result<Queue<T>> GetNextQueue()
-		{
-			foreach (var queue in _internalQueues.Values)
-			{
-				try
-				{
-					queue.Peek();
-					return Result.Ok(queue);
-				}
-				catch (Exception) { }
-			}
+        private Result<Queue<T>> GetNextQueue()
+        {
+            foreach (var queue in _internalQueues.Values)
+            {
+                try
+                {
+                    queue.Peek();
+                    return Result.Ok(queue);
+                }
+                catch (Exception) { }
+            }
 
-			return Result.Fail<Queue<T>>("Could not find a queue with items.");
-		}
-	}
+            return Result.Fail<Queue<T>>("Could not find a queue with items.");
+        }
+    }
 }
---
+++ ConcurrentPriorityQueueTests/PriorityQueueIEnumerabeUnitTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+using FluentAssertions;
+using ConcurrentPriorityQueue;
+
+namespace ConcurrentPriorityQueueTests
+{
+	/// <summary>
+	/// Test general functionalty of the queue [Enqueue, Dequeue, Peek]
+	/// </summary>
+	public class PriorityQueueIEnumerabeUnitTests
+	{			
+		private const int supportedNumberOfPriorites = 3;
+		private readonly IConcurrentPriorityQueue<IHavePriority> _targetQueue;
+
+		public PriorityQueueIEnumerabeUnitTests()
+		{
+			_targetQueue = new ConcurrentPriorityQueue<IHavePriority>(supportedNumberOfPriorites);
+		}
+
+		public static IEnumerable<object[]> MockItemsWithPriority =>
+			new List<object[]>
+			{
+				new object[] { new MockWithPriority(0) },
+				new object[] { new MockWithPriority(1) },
+				new object[] { new MockWithPriority(2) },
+			};
+
+		[Fact]
+		public void GetEnumerator_QueueContainsDifferentPriorities_EnumerationIsOrderedByPriority()
+		{
+			// Arrange
+			MockItemsWithPriority.ToList().ForEach(items => _targetQueue.Enqueue(items[0] as IHavePriority));
+
+			// Assert
+			var expectedPriority = 0;
+			foreach (var item in _targetQueue)
+			{
+				item.Priority.Should().Be(expectedPriority++);
+			}
+		}
+
+		[Fact]
+		public void GetEnumerator_QueueContainsSamePriorities_EnumerationIsOrderedByFirstIn()
+		{
+			// Arrange
+			var mockWithPriority1 = new MockWithPriority(0);
+			var mockWithPriority2 = new MockWithPriority(0);
+
+			_targetQueue.Enqueue(mockWithPriority1);
+			_targetQueue.Enqueue(mockWithPriority2);
+
+			// Act
+			var enumerator = _targetQueue.GetEnumerator();
+
+			// Assert
+			enumerator.MoveNext();
+			enumerator.Current.Should().Be(mockWithPriority1);
+			enumerator.MoveNext();
+			enumerator.Current.Should().Be(mockWithPriority2);
+		}
+
+		[Fact]
+		public void Count__ReturnsAggregatedCount()
+		{
+			// Arrange
+			MockItemsWithPriority.ToList().ForEach(items => _targetQueue.Enqueue(items[0] as IHavePriority));
+
+			// Act
+			var totalCount = _targetQueue.Count;
+
+			// Assert
+			totalCount.Should().Be(MockItemsWithPriority.ToList().Count);
+		}
+	}
+}
