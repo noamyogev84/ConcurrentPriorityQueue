1918f3fc9ff95a436cee1b8113ea39f5ba34a5d0
Ralf Jänike <jaenike@ferrari-electronic.de>
2020-08-17 08:16:25 +0200

    consider Queue-Capacity when "casting" ToBlockingCollection additionally: another extension Method with capacity argument

--- ConcurrentPriorityQueue/Core/ConcurrentPriorityQueueExtensions.cs
+++ ConcurrentPriorityQueue/Core/ConcurrentPriorityQueueExtensions.cs
@@ -1,12 +1,31 @@
-﻿using System.Collections.Concurrent;
-using System;
+﻿using System;
+using System.Collections.Concurrent;
 
 namespace ConcurrentPriorityQueue.Core
 {
-	public static class ConcurrentPriorityQueueExtensions
-	{
-		public static BlockingCollection<T> ToBlockingCollection<T, TP>(this IConcurrentPriorityQueue<T, TP> queue)
-			where T : IHavePriority<TP>
-			where TP : IEquatable<TP>, IComparable<TP> => new BlockingCollection<T>(queue);
-	}
+    public static class ConcurrentPriorityQueueExtensions
+    {
+        public static BlockingCollection<T> ToBlockingCollection<T, TP>(this IConcurrentPriorityQueue<T, TP> queue)
+            where T : IHavePriority<TP>
+            where TP : IEquatable<TP>, IComparable<TP>
+        {
+            if (queue.Capacity > 0)
+            {
+                return new BlockingCollection<T>(queue, queue.Capacity);
+            }
+            return new BlockingCollection<T>(queue);
+        }
+
+        public static BlockingCollection<T> ToBlockingCollection<T, TP>(this IConcurrentPriorityQueue<T, TP> queue, int capacity)
+            where T : IHavePriority<TP>
+            where TP : IEquatable<TP>, IComparable<TP>
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("capacity");
+            }
+            return new BlockingCollection<T>(queue, capacity);
+
+        }
+    }
 }
